d0fb30c84045c1070e7f54c083b02076964bf4dc
Sandun <sandun89@gmail.com>
2025-06-19 21:57:54 +0530

    admin orders view page updated 2️⃣

--- src/pages/admin/adminOrders.jsx
+++ src/pages/admin/adminOrders.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Loader from "../../components/loader";
 import { MdClose } from "react-icons/md";
+import { IoEyeOutline } from "react-icons/io5";
 
 export default function AdminOrders(){
 
@@ -27,7 +28,20 @@ export default function AdminOrders(){
             }
         }, [loaded]
     );
-    
+
+    function changeOrderStatus(orderId, status){
+      const token = localStorage.getItem("authToken");
+      axios.put(import.meta.env.VITE_BACKEND_URL + "/api/order/" + orderId, {
+        status: status
+      },{
+        headers: {
+          Authorization: "Bearer " + token
+        }
+      }).then(()=>{
+        toast.success("Order Status Changed Successfully")
+      })
+    }
+     
     return(
         <div className="w-full h-full">
             {
@@ -44,6 +58,7 @@ export default function AdminOrders(){
                                 <th className="p-2">Status</th>
                                 <th className="p-2">Total</th>
                                 <th className="p-2">Date</th>
+                                <th className="p-2">Action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -53,10 +68,6 @@ export default function AdminOrders(){
                                         return (
                                           <tr
                                             key={index}
-                                            onClick={() => {
-                                              setModalDisplay(true);
-                                              setDisplayOrder(order);
-                                            }}
                                             className="border-b-[2px] border-gray-500 cursor-pointer hover:bg-gray-100"
                                           >
                                             <td className="p-2">
@@ -75,7 +86,20 @@ export default function AdminOrders(){
                                               {order.phoneNumber}
                                             </td>
                                             <td className="p-2">
-                                              {order.status}
+                                              <select onChange={ (evt)=> {}} value={order.status}>
+                                                <option value="Pending">
+                                                  Pending
+                                                </option>
+                                                <option value="Processing">
+                                                  Processing
+                                                </option>
+                                                <option value="Delivered">
+                                                  Delivered
+                                                </option>
+                                                <option value="Cancelled">
+                                                  Cancelled
+                                                </option>
+                                              </select>
                                             </td>
                                             <td className="p-2">
                                               {order.total.toFixed(2)}
@@ -84,6 +108,17 @@ export default function AdminOrders(){
                                               {new Date(
                                                 order.date
                                               ).toDateString()}
+                                            </td>
+                                            <td className="p-2">
+                                              <button
+                                                onClick={() => {
+                                                  setModalDisplay(true);
+                                                  setDisplayOrder(order);
+                                                }}
+                                                className="text-2xl text-white bg-blue-600 p-[4px] rounded border border-blue-600 cursor-pointer hover:bg-blue-100 hover:text-black"
+                                              >
+                                                <IoEyeOutline />
+                                              </button>
                                             </td>
                                           </tr>
                                         );
@@ -99,13 +134,28 @@ export default function AdminOrders(){
                                 <button onClick={()=>{setModalDisplay(false)}} className="w-[40px] h-[40px] absolute top-[-20px] right-[-20px] flex justify-center items-center cursor-pointer rounded-full border-[1px] border-red-500 p-[10px] bg-red-500 text-white hover:bg-red-100 hover:text-black">
                                     <MdClose />
                                 </button>
-                                <div className="w-full h-[150px] text-lg">
+                                <div className="w-full h-[150px] p-[5px] text-lg">
                                     <h1 className="py-[4px]">Order ID: {displayOrder.orderId}</h1>
                                     <h1 className="py-[4px]">Order Date: {new Date(displayOrder.date).toDateString()}</h1>
                                     <h1 className="py-[4px]">Order Status: {displayOrder.status}</h1>
                                     <h1 className="py-[4px]">Order Total: {displayOrder.total.toFixed(2)}</h1>
                                 </div>
-                                <div className="w-full h-[450px] bg-red-200"></div>
+                                <div className="w-full h-[450px] max-h-[450px] p-[10px] overflow-y-scroll">
+                                  {
+                                    displayOrder.billItems.map((item, index) => {
+                                      return(
+                                        <div key={index} className="w-full h-[100px] my-[5px] flex bg-white shadow-2xl">
+                                          <img src={item.image} className="w-[100px] h-full aspect-square object-cover" alt="" />
+                                          <div className="w-[450px] h-full ms-[10px]">
+                                            <h1 className="text-xl">{item.productName}</h1>
+                                            <h1 className="text-lg">{item.price.toFixed(2)}</h1>
+                                            <h1 className="text-lg">{item.quantity}</h1>
+                                          </div>
+                                        </div>
+                                      );
+                                    })
+                                  }
+                                </div>
                             </div>
                         </div>
                     }
