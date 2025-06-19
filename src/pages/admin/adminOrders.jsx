c6c401fad7f39cfa924875213c321c2857feed20
Sandun Kumara <sandun89@gmail.com>
2025-06-19 16:07:21 +0530

    admin order view page updated 2️⃣

--- src/pages/admin/adminOrders.jsx
+++ src/pages/admin/adminOrders.jsx
@@ -8,6 +8,7 @@ export default function AdminOrders(){
     const[orders, setOrders] = useState([]);
     const[loaded, setLoaded] = useState(false);
     const[modalIsDisplay, setModalDisplay] = useState(false);
+    const[displayOrder, setDisplayOrder] = useState(null);
 
     useEffect(
         () => {
@@ -49,17 +50,42 @@ export default function AdminOrders(){
                             {
                                 orders.map(
                                     (order, index) => {
-                                        return(
-                                            <tr key={index} onClick={ () => { setModalDisplay(true) }} className="border-b-[2px] border-gray-500 cursor-pointer hover:bg-gray-100">
-                                                <td className="p-2">{order.orderId}</td>
-                                                <td className="p-2">{order.email}</td>
-                                                <td className="p-2">{order.name}</td>
-                                                <td className="p-2">{order.address}</td>
-                                                <td className="p-2">{order.phoneNumber}</td>
-                                                <td className="p-2">{order.status}</td>
-                                                <td className="p-2">{order.total.toFixed(2)}</td>
-                                                <td className="p-2">{new Date(order.date).toDateString()}</td>
-                                            </tr>
+                                        return (
+                                          <tr
+                                            key={index}
+                                            onClick={() => {
+                                              setModalDisplay(true);
+                                              setDisplayOrder(order);
+                                            }}
+                                            className="border-b-[2px] border-gray-500 cursor-pointer hover:bg-gray-100"
+                                          >
+                                            <td className="p-2">
+                                              {order.orderId}
+                                            </td>
+                                            <td className="p-2">
+                                              {order.email}
+                                            </td>
+                                            <td className="p-2">
+                                              {order.name}
+                                            </td>
+                                            <td className="p-2">
+                                              {order.address}
+                                            </td>
+                                            <td className="p-2">
+                                              {order.phoneNumber}
+                                            </td>
+                                            <td className="p-2">
+                                              {order.status}
+                                            </td>
+                                            <td className="p-2">
+                                              {order.total.toFixed(2)}
+                                            </td>
+                                            <td className="p-2">
+                                              {new Date(
+                                                order.date
+                                              ).toDateString()}
+                                            </td>
+                                          </tr>
                                         );
                                     }
                                 )
@@ -73,7 +99,12 @@ export default function AdminOrders(){
                                 <button onClick={()=>{setModalDisplay(false)}} className="w-[40px] h-[40px] absolute top-[-20px] right-[-20px] flex justify-center items-center cursor-pointer rounded-full border-[1px] border-red-500 p-[10px] bg-red-500 text-white hover:bg-red-100 hover:text-black">
                                     <MdClose />
                                 </button>
-                                <div className="w-full h-[150px] bg-amber-200"></div>
+                                <div className="w-full h-[150px] text-lg">
+                                    <h1 className="py-[4px]">Order ID: {displayOrder.orderId}</h1>
+                                    <h1 className="py-[4px]">Order Date: {new Date(displayOrder.date).toDateString()}</h1>
+                                    <h1 className="py-[4px]">Order Status: {displayOrder.status}</h1>
+                                    <h1 className="py-[4px]">Order Total: {displayOrder.total.toFixed(2)}</h1>
+                                </div>
                                 <div className="w-full h-[450px] bg-red-200"></div>
                             </div>
                         </div>
